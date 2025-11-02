5e83e97f9d6e55ed9c739085bd63272abfd9afd5
SEUNGMIN KIM <231153824+kehne100-pixel@users.noreply.github.com>
2025-11-02 14:03:43 +0900

    캐릭터 이동구현 뼈대 함수 완성

---
+++ main.py
@@ -0,0 +1,27 @@
+from pico2d import *
+
+open_canvas(800, 600)
+
+character = load_image('Keroro_Sheet.png')
+
+
+running = True
+x, y = 400, 300
+frame = 0
+dir_x, dir_y = 0, 0
+look_dir = 0
+
+def handle_events():
+
+
+
+
+
+
+
+while running:
+    clear_canvas()
+
+
+
+
