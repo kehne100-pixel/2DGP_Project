d53bb7713d7c89fe557a4d6596cb7ec053066c4f
SEUNGMIN KIM <231153824+kehne100-pixel@users.noreply.github.com>
2025-11-05 15:04:31 +0900

    오류 수정

--- main.py
+++ main.py
@@ -12,6 +12,35 @@ dir_x, dir_y = 0, 0
 look_dir = 0
 
 def handle_events():
+    global running, dir_x, dir_y, look_dir, dir_x, dir_y, frame, last_dir
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir_x += 1
+                last_dir = 1
+            elif event.key == SDLK_LEFT:
+                dir_x -= 1
+                last_dir = -1
+            elif event.key == SDLK_UP:
+                dir_y += 1
+
+            elif event.key == SDLK_DOWN:
+                dir_y -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir_x -= 1
+            elif event.key == SDLK_LEFT:
+                dir_x += 1
+            elif event.key == SDLK_UP:
+                dir_y -= 1
+            elif event.key == SDLK_DOWN:
+                dir_y += 1
+
 
 
 
@@ -21,6 +50,10 @@ def handle_events():
 
 while running:
     clear_canvas()
+
+
+
+
 
 
 
