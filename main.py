ca966e8b7380c34f62b6bef952bf255ce7a7b650
SEUNGMIN KIM <231153824+kehne100-pixel@users.noreply.github.com>
2025-11-10 00:00:18 +0900

    수업내용 업데이트 추가

--- main.py
+++ main.py
@@ -1,29 +1,67 @@
-# main.py
 from pico2d import *
-from Keroro import Boy
+from Keroro import keroro
+import play_mode as start_mode
+import game_framework
 
-def handle_events(boy):
-    events = get_events()
-    for e in events:
-        if e.type == SDL_QUIT:
-            return False
-        if e.type == SDL_KEYDOWN and e.key == SDLK_ESCAPE:
-            return False
-        boy.handle_event(e)
-    return True
+open_canvas()
+game_framework.run(start_mode)
+close_canvas()
 
-def main():
-    open_canvas(800, 600)
-    boy = Boy()
-    running = True
-    while running:
-        running = handle_events(boy)
-        clear_canvas()
-        boy.update()
-        boy.draw()
-        update_canvas()
-        delay(0.01)
-    close_canvas()
 
-if __name__ == '__main__':
-    main()
+
+# Game object class here
+
+
+def handle_events():
+    global running
+
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            keroro.handle_event(event)
+
+
+def reset_world():
+    global world
+    global keroro
+
+    world = []
+
+
+
+
+    world.append(keroro)
+
+
+
+def update_world():
+    for o in world:
+        o.update()
+    pass
+
+
+def render_world():
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
+
+
+running = True
+
+
+
+open_canvas()
+reset_world()
+# game loop
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.1)
+# finalization code
+close_canvas()
