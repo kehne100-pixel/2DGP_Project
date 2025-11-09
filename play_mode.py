5b92441b7e5c914c92e02cbb1bb72ebf933565ad
SEUNGMIN KIM <231153824+kehne100-pixel@users.noreply.github.com>
2025-11-10 00:00:09 +0900

    수업내용 업데이트 추가

---
+++ play_mode.py
@@ -0,0 +1,48 @@
+from pico2d import *
+
+import game_framework
+import item_mode
+import title_mode
+from boy import Boy
+from grass import Grass
+import game_world
+
+def handle_events():
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_mode(title_mode)
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_i:
+            game_framework.push_mode(item_mode)
+        else:
+            boy.handle_event(event)
+
+def init():
+    global boy
+
+    grass = Grass()
+    game_world.add_object(grass, 0)
+
+    boy = Boy()
+    game_world.add_object(boy, 1)
+
+def finish():
+    game_world.clear()
+
+def update():
+    game_world.update()
+
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+def pause():
+    pass
+
+def resume():
+    pass
+
