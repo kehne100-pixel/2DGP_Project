5179adac6683cb704513c4b34ff300a580479bf5
SEUNGMIN KIM <231153824+kehne100-pixel@users.noreply.github.com>
2025-11-11 20:02:02 +0900

    logo 화면 뼈대 구현

---
+++ logo_mode.py
@@ -0,0 +1,22 @@
+import game_framework
+from pico2d import *
+
+def init():
+    global image
+    global running
+    global logo_start_time
+
+    image = load_image('Keroro_logo.png')
+    running = True
+    logo_start_time = get_time()
+
+def finish():
+    global load_image
+    del image
+
+def update():
+    global logo_start_time
+    if get_time() - logo_start_time >= 2.0:
+        logo_start_time = get_time()
+        game_framework.quit()
+
