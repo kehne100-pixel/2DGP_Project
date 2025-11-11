a2c6218e504c043cb4a09e2a8ff72640ea5bbe1b
SEUNGMIN KIM <231153824+kehne100-pixel@users.noreply.github.com>
2025-11-11 20:08:05 +0900

    오류 수정

--- logo_mode.py
+++ logo_mode.py
@@ -18,5 +18,5 @@ def update():
     global logo_start_time
     if get_time() - logo_start_time >= 2.0:
         logo_start_time = get_time()
-        game_framework.quit()
+        game_framework.change_mode(title_mode)
 
