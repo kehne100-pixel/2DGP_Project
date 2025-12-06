eedf6432209d359835dbbd67d8b8a8b2775a739d
SEUNGMIN KIM <231153824+kehne100-pixel@users.noreply.github.com>
2025-12-06 17:47:38 +0900

    충돌처리 파일 생성 및 뼈대 함수 구현

---
+++ fight_collision.py
@@ -0,0 +1,16 @@
+# fight_collision.py
+from pico2d import draw_rectangle
+
+DEBUG_COLLISION = True  # 디버그용 박스 보고 싶지 않으면 False 로
+
+
+def aabb_collide(bb1, bb2):
+
+
+
+def draw_bb(bb):
+
+
+def handle_fight_collision(player, enemy):
+
+
