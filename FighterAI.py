e1401d03af2926bb5878258eb361d78449e396de
SEUNGMIN KIM <231153824+kehne100-pixel@users.noreply.github.com>
2025-12-06 14:39:00 +0900

    AI객체 생성 및 AI기준 스킬 및 기본공격 적용

--- FighterAI.py
+++ FighterAI.py
@@ -9,26 +9,45 @@ from sdl2 import (
     SDLK_1, SDLK_2, SDLK_3,
 )
 
-# 아주 단순한 더미 이벤트 객체 (키보드 이벤트 흉내내기용)
+
 class DummyEvent:
     def __init__(self, type, key):
-
+        self.type = type
+        self.key = key
 
 
 class FighterAI:
     def __init__(self, enemy, player):
 
+        self.enemy = enemy
+        self.player = player
+
+        # 이동 상태 관리
+        self.current_move_key = None  # SDLK_RIGHT / SDLK_LEFT / None
+
+        # 가드 상태 관리
+        self.guard_active = False
+        self.guard_timer = 0.0
+
+        # 의사결정 쿨타임
+        self.decision_timer = 0.0
+
+        # 공격1/공격2 번갈아 사용
+        self.use_attack1_next = True
+
+        # 거리 기준 (상황에 맞게 나중에 조정 가능)
+        self.APPROACH_DISTANCE = 350   # 이보다 멀면 접근
+        self.CLOSE_RANGE       = 150   # 근접 공격/스킬1,2 사용 거리
+        self.SKILL3_RANGE      = 300   # 필살기 스킬3은 좀 더 먼 거리에서도 사용
 
 
 
     def _send_keydown(self, key):
 
-
     def _send_keyup(self, key):
 
 
     def _start_move(self, direction):
-
 
     def _stop_move(self):
 
@@ -41,12 +60,10 @@ class FighterAI:
 
     def _do_attack1(self):
 
-
     def _do_attack2(self):
 
 
     def _do_skill1(self):
-
 
     def _do_skill2(self):
 
@@ -57,3 +74,4 @@ class FighterAI:
     # ---------- 메인 업데이트 ----------
 
     def update(self):
+
